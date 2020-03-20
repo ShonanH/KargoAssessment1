43c4980385338705f65688a3509e474b4ca51eb2
ShonanH <53867304+ShonanH@users.noreply.github.com>
2020-03-20 17:19:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-
+def isIsomorphic(s, t) -> bool:
+    if (len(s) != len(t)):
+        return (False)
+    return getIso(s,t) and getIso(t,s)
+    
+def getIso(s, t):
+    #print (t)
+    len_s = len(s)
+    len_t = len(t)
+    check = {} #create a dictionary
+    for i in range(len_s):
+        if s[i] not in check:
+            #print (check)
+            #print (s[i])
+            check[s[i]] = t[i]
+            #print(check)
+        elif check[s[i]] != t[i]:
+            return (False)
+    return (True)
+print(isIsomorphic("foo", "321"))
